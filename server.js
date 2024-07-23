fd98e3c93b1212807259a109fc0117bce7b895e8
pratap-5 <142750642+pratap-5@users.noreply.github.com>
2024-07-23 17:37:59 +0530

    link added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMogodb from "./db/connectToMongodb.js";
 import uploadRouter from "./routes/upload.routes.js";
-import profileRouter from "./routes/profile.routes.js";
+import profileRouter from "./routes/profile.routes.js"
 import path from "path";
 
 dotenv.config();
@@ -18,8 +18,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "*", // Allow all origins
-
+    origin: "https://friend-xav2.onrender.com", // Allow this origin
     credentials: true, // Allow credentials (cookies, headers)
   })
 );
@@ -30,7 +29,7 @@ app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 app.use("/api/auth", authRoutes);
 app.use("/api/uploads", uploadRouter);
 
-app.use("/api/profile", profileRouter);
+app.use("/api/profile",profileRouter)
 
 app.listen(port, () => {
   connectToMogodb();
